c15653f2cf7b36735955967fe51aaf2cf3c29dc8
Pranish Shrestha <pranish580@gmail.com>
2024-08-01 16:06:27 +0530

    Update login.component.ts

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Route, Router } from '@angular/router';
+import { zip } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -24,9 +25,15 @@ export class LoginComponent {
   }
 
   onLogin() {
-    this.http.post("", this.loginObj).subscribe((res:any)=>{
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+    this.http.post("http://localhost:8000/api/auth/login/", this.loginObj, { headers }).subscribe((res:any)=>{
       debugger;
       if(res.result) {
+        // Store JWT tokens in local storage
+        localStorage.setItem('access', res.access);
+        localStorage.setItem('refresh', res.refresh);
+        
         alert("Login Successful");
         this.router.navigateByUrl("dashboard")
       }
