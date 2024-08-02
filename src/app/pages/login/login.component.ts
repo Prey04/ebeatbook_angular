298d8c0f602a5d025f83598b350b3b50d3499ab0
Pranish Shrestha <pranish580@gmail.com>
2024-08-02 16:19:27 +0530

    angular

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,45 +1,54 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Route, Router } from '@angular/router';
-import { zip } from 'rxjs';
+import { Router } from '@angular/router';
+import { LocalstorageService } from '../../services/localstorage.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
 
   loginObj: any = {
-    "username": "",
+    "email": "",
     "password": ""
   };
   
   http = inject(HttpClient);
 
-  constructor (private router:Router) {
-    
-  }
+  constructor(private router: Router) {}
 
   onLogin() {
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
 
-    this.http.post("http://localhost:8000/api/auth/login/", this.loginObj, { headers }).subscribe((res:any)=>{
-      debugger;
-      if(res.result) {
-        // Store JWT tokens in local storage
-        localStorage.setItem('access', res.access);
-        localStorage.setItem('refresh', res.refresh);
-        
-        alert("Login Successful");
-        this.router.navigateByUrl("dashboard")
+    this.http.post("http://127.0.0.1:8000/api/login/", this.loginObj, { headers }).subscribe({
+      next: (res: any) => {
+        console.log('Response:', res);
+        const authenticateduser = res.authenticatedUser
+        if (authenticateduser.access && authenticateduser.refresh) {
+
+          const access = authenticateduser.access;
+          const refresh = authenticateduser.refresh;
+
+          localStorage.setItem('access', access);
+          localStorage.setItem('refresh', refresh);
+
+
+          
+          alert("Login Successful");
+          this.router.navigateByUrl("dashboard");
+        } else {
+          alert("Check Username or Password");
+        }
+      },
+      error: (error) => {
+        console.error('Error:', error);
+        alert("An error occurred. Please try again.");
       }
-      else {
-        alert("Check Email Id or Password")
-      }
-    })
+    });
   }
 }
